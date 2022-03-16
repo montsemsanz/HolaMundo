0e2d92a7cb9c538aff8dd68f36fb46dc21404999
montsemsanz <montsemsanz@gmail.com>
2022-03-16 20:59:59 +0100

    Definida variable suma

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,5 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hola mundo desde rama dev-montse");
+        int suma = 20;
+        System.out.println("Suma = " + suma);
     }
 }
